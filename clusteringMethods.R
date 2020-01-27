87464c7ae4e26a4391a37a87a279121f129f4536
PSzymon <pol.szym95@gmail.com>
2020-01-27 23:12:00 +0100

    Divisive hierarchical method (diana), manhattan and euclidean

--- clusteringMethods.R
+++ clusteringMethods.R
@@ -22,12 +22,26 @@ clustering.agnes <- function(metric)
   return(result)
 }
 
+# Divisive hierarchical (top-down)
+clustering.diana <- function(metric)
+{
+  cl.diana <- diana(x = iris, metric = metric)
+  plot(cl.diana, which.plots = 2)
+  result <- cutree(cl.diana, 3)
+  
+  return(result)
+}
+
 METRICS <- get.metrics.enum()
 kmeans.result <- clustering.kmeans(nOfCenters = nCenters)
 agnes.result.manhattan <- clustering.agnes(metric = METRICS$MANHATTAN)
 agnes.result.euclidean <- clustering.agnes(metric = METRICS$EUCLIDEAN)
+diana.result.manhattan <- clustering.diana(metric = METRICS$MANHATTAN)
+diana.result.euclidean <- clustering.diana(metric = METRICS$EUCLIDEAN)
 
 result <- iris
 result <- cbind(result, kmeans = kmeans.result)
 result <- cbind(result, agnes.man = agnes.result.manhattan)
 result <- cbind(result, agnes.euc = agnes.result.euclidean)
+result <- cbind(result, diana.man = diana.result.manhattan)
+result <- cbind(result, diana.euc = diana.result.euclidean)
