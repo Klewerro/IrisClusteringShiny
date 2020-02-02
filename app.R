abfd4b320956798c97fee40dc5f63cad0f6d36bc
PSzymon <pol.szym95@gmail.com>
2020-02-02 18:11:00 +0100

    Shiny application basic UI. Displaying table data

--- app.R
+++ app.R
@@ -9,40 +9,41 @@
 
 library(shiny)
 
-# Define UI for application that draws a histogram
+source("clusteringMethods.R")
+source("supportMethods.R")
+source("dataVisualization.R")
+
+# UI
 ui <- fluidPage(
 
     # Application title
     titlePanel("Old Faithful Geyser Data"),
 
-    # Sidebar with a slider input for number of bins 
     sidebarLayout(
         sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
+            selectInput("xattribute", "First attribute (x)", c("Sepal Length", "Sepal Width", "Petal Length", "Petal Width")),
+            selectInput("yattribute", "Second attribute", c("Sepal Length", "Sepal Width", "Petal Length", "Petal Width")),
+            actionButton("button", "Clusterize")
         ),
 
-        # Show a plot of the generated distribution
         mainPanel(
-           plotOutput("distPlot")
+           plotOutput("distPlot"),
+           tableOutput("tableData")
         )
     )
 )
 
-# Define server logic required to draw a histogram
+# Server logic
 server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    
+    resultKMeans <- eventReactive(input$button, {
+        result <- iris
+        kmeans.result <- clustering.kmeans(nOfCenters = nCenters)
+        result <- cbind(result, Kmeans = kmeans.result)
+        return(result)
     })
+    
+    output$tableData <- renderTable(resultKMeans())
 }
 
 # Run the application 
